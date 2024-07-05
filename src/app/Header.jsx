4a7c01aac8d37e4d77f0f61e92925b5cf5387bc3
masiham <masihmooohmmadpoor@gmail.com>
2024-07-05 11:06:57 +0330

    created Header

---
+++ src/app/Header.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <section>
+      <header className="flex justify-between p-4 px-20 glassmorphism text--white">
+        <ul className="flex items-center justify-between w-32">
+          <li><Link href="/">خانه</Link></li>
+          <li><Link href="#">محصولات</Link></li>
+        </ul>
+        <div className="flex items-center justify-between w-24">
+          <button><Link href="/signin">ورود</Link></button>
+          <span> / </span>
+          <button><Link href="/signup">ثبت نام</Link></button>
+        </div>
+      </header>
+    </section>
+  );
+};
+
+export default Header;
---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "./Header";
+
+export default function Home() {
+  
+  return (
+    <>
+    <Header/>
+    <main></main>
+    </>
+  )
+}
