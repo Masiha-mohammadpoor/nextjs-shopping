43fd964e0c7b4039446f6d2a782e5f046bd38789
masiham <masihmooohmmadpoor@gmail.com>
2024-07-10 13:48:15 +0330

    created next middleware

---
+++ src/middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import middlewareAuth from "./utils/middlewareAuth";
+
+export async function middleware(req) {
+  const url = req.url;
+  const pathname = req.nextUrl.pathname;
+  const user = await middlewareAuth(req);
+
+  if (pathname.startsWith("/profile")) {
+    if (!user) return NextResponse.redirect(new URL("/auth", url));
+  }
+  if (pathname.startsWith("/admin")) {
+    if (!user) return NextResponse.redirect(new URL("/auth", url));
+    if(user && user.role !== "ADMIN") {
+      return NextResponse.redirect(new URL("/", url));
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/admin:path*", "/profile:path*"],
+};
