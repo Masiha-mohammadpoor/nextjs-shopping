0ef9e21403f65452c2acde0889fee4cf0843f6e1
masiham <masihmooohmmadpoor@gmail.com>
2024-07-10 21:03:25 +0330

    created route group

--- src/app/(admin)/admin/layout.jsx
+++ src/app/(admin)/admin/layout.jsx
@@ -1,8 +1,8 @@
-import "./globals.css";
+import "../../globals.css";
 import vazirFont from "@/constants/localFonts";
-import Header from "../components/Header";
+import Header from "../../../components/Header";
 import { Toaster } from "react-hot-toast";
-import Providers from "./providers";
+import Providers from "../../providers";
 export const metadata = {
   title: "Next Shop Panel",
   description: "Next.js Course Fronthooks Course",
---
+++ src/app/(profile)/profile/layout.jsx
@@ -0,0 +1,23 @@
+import "../../globals.css";
+import vazirFont from "@/constants/localFonts";
+import Header from "../../../components/Header";
+import { Toaster } from "react-hot-toast";
+import Providers from "../../providers";
+export const metadata = {
+  title: "Next Shop Panel",
+  description: "Next.js Course Fronthooks Course",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body className={`${vazirFont.variable} font-sans`}>
+        <Providers>
+        <Header />
+          <main>{children}</main>
+        </Providers>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
