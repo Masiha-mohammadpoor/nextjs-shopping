81b13eff59a84e2b3e0a1a463ec5e5f4fc71edb9
masiham <masihmooohmmadpoor@gmail.com>
2024-07-14 13:00:23 +0330

    added category query to url

--- src/app/(user)/products/CategorySidebar.jsx
+++ src/app/(user)/products/CategorySidebar.jsx
@@ -1,18 +1,49 @@
-"use client"
+"use client";
 import CheckBox from "@/common/CheckBox";
-import { useState } from "react";
+// import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useState } from "react";
+import { usePathname } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const CategorySidebar = ({ categories }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  const [selectedCategories , setSelectedCategories] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState(
+    searchParams.get("category").split(",") || []
+  );
+
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
 
   const categoryHandler = (e) => {
-    if(e.target.checked) setSelectedCategories([...selectedCategories , e.target.value]);
-    else {
-      const filteredArray = selectedCategories.filter(category => category !== e.target.value);
-      setSelectedCategories(filteredArray);
-    }
-  }
+    if (e.target.checked) {
+      setSelectedCategories([...selectedCategories, e.target.value]);
+      router.push(
+        pathname +
+          "?" +
+          createQueryString("category", [...selectedCategories, e.target.value])
+      );
+    } else { 
+
+        const filteredArray = selectedCategories.filter(
+          (category) => category !== e.target.value
+        );
+        setSelectedCategories(filteredArray);
+        router.push(
+          pathname + "?" + createQueryString("category", filteredArray)
+        );
+  };
+}
 
   return (
     <article className="col-span-1">
